08a031d1c02ccb40c53e4068e2a966c4eaeede8d
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-05 15:57:28 -0500

    Update postRouter.js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,23 +2,16 @@ const express = require("express");
 const router = express.Router();
 const posts = require("./postDb");
 
+//* READ ALL POSTS
 router.get("/", (req, res) => {});
-
+//* READ POSTS BY ID
 router.get("/:id", validatePostId, (req, res) => {});
-
+//* UPDATE POST BY ID
+router.put("/:id", validatePostId, (req, res) => {});
+//* DELETE POST BY ID
 router.delete("/:id", validatePostId, (req, res) => {});
 
-router.put("/:id", validatePostId, (req, res) => {});
-
 //* custom VALIDATE middleware
-function validatePostId(req, res, next) {
-  try {
-    posts.getById(req.params.id).then(response => {
-      response ? next() : res.status(404).json({ message: "invalid user id" });
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Could not validate post id" });
-  }
-}
+function validatePostId(req, res, next) {}
 
 module.exports = router;
