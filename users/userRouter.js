0fc0f40330e1e3e417d7c452986e44f8bd021f67
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-05 15:57:34 -0500

    Update userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,27 +1,134 @@
 const express = require("express");
 const router = express.Router();
 const users = require("./userDb");
+const posts = require("../posts/postDb");
 
-router.post("/", (req, res) => {});
+//* CREATE USER
+router.post("/", validateUser, (req, res) => {
+  users
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ Error: "There was an issue adding the user to the database" });
+    });
+});
+//* CREATE POST
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
+  req.body = { ...req.body, user_id: req.user.id };
+  posts
+    .insert(req.body)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ Error: "There was an issue adding the post to the database" });
+    });
+});
+//* READ ALL USERS
+router.get("/", (req, res) => {
+  users
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ Error: "Internal server error while trying to request users" });
+    });
+});
+//* READ USER BY ID
+router.get("/:id", validateUserId, (req, res) => {
+  users
+    .getById(req.user.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ Error: "there was an error getting the user from the server" });
+    });
+});
+//* READ USER'S POSTS BY ID
+router.get("/:id/posts", validateUserId, (req, res) => {
+  users
+    .getUserPosts(req.user.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({
+        Error:
+          "there was an issue retriveing the posts for that user from the server"
+      });
+    });
+});
+//* UPDATE USER BY ID
+router.put("/:id", validateUserId, validateUser, (req, res) => {
+  users
+    .update(req.user.id, req.body)
+    .then(num => {
+      if (num == 1) {
+        res.status(200).json({ message: "the user was updated succesfully" });
+      } else {
+        res
+          .status(500)
+          .json({ message: "There was an error updateing the user" });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "There was an error updateing the user" });
+    });
+});
+//* DELETE USER BY ID
+router.delete("/:id", validateUserId, (req, res) => {
+  users
+    .remove(req.user.id)
+    .then(num => {
+      res.status(200).json({ Items_deleted: num });
+    })
+    .catch(() => {
+      res.status(500).json({ Error: "there was an error deleteing the user" });
+    });
+});
 
-router.post("/:id/posts", (req, res) => {});
+//* CUSTOM MIDDLEWARE
+function validateUserId(req, res, next) {
+  users.getById(req.params.id).then(userWithId => {
+    if (userWithId == undefined) {
+      res.status(400).json({ message: "invalid user id" });
+    } else {
+      req.user = userWithId;
+      next();
+    }
+  });
+}
 
-router.get("/", (req, res) => {});
+function validateUser(req, res, next) {
+  if (Object.keys(req.body).length == 0) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (req.body.name == null) {
+    res.status(400).json({ message: "missing required name field" });
+  }
+  next();
+}
 
-router.get("/:id", (req, res) => {});
-
-router.get("/:id/posts", (req, res) => {});
-
-router.delete("/:id", (req, res) => {});
-
-router.put("/:id", (req, res) => {});
-
-//custom middleware
-
-function validateUserId(req, res, next) {}
-
-function validateUser(req, res, next) {}
-
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  if (Object.keys(req.body).length == 0) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (req.body.text == null) {
+    res.status(400).json({ message: "missing required text field" });
+  }
+  next();
+}
 
 module.exports = router;
