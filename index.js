61ed3ad1321b46ac7825c2423eb5b316c26e359d
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-05 15:57:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const server = require("./server");
 const port = 6000;
 
-server.get("/", (req, res) => {
-  res.send("Let's write some middleware!");
-});
-
-// 404 Fallback
-server.use(function(req, res) {
-  res.status(404).send("😿 RIP this endpoint 😿");
-});
-
-server.listen(port, () => console.log(`API on port ${port}...`));
+server.listen(port, () => console.log(`API running on port ${port}...`));
